0c026924ea32ba9193b31de3286150140a7af2e8
David Marinho <david.marinho@codacy.com>
2022-04-28 11:32:19 +0100

    modified workflow

--- calc_test.go
+++ calc_test.go
@@ -4,11 +4,15 @@ import (
     "testing"
 )
 
-func TestHelloName(t *testing.T) {
+func TestCalculator(t *testing.T) {
     num1 := 2
 	num2 := 3
     sum := sumNum(num1,num2)
+    subtract := subNum(num1,num2)
     if sum != 5 {
         t.Fatalf("Sum is invalid. It should be %d but the sum is %d", 5, sum)
+    }
+    if subtract != 1 {
+        t.Fatalf("Subtraction is invalid. It should be %d but the subtraction is %d", 5, sum)
     }
 }
--- calculator.go
+++ calculator.go
@@ -1,20 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main(){
 	fmt.Printf("Sum: %d\n",sumNum(2,3))
-	fmt.Printf("Credential: %s\n",credential())
+	credential()
 }
 
 func sumNum(num1 int, num2 int) int {
 	return num1+num2;
 }
 
-func credential() string {
-	return "312321321321321";
+func credential() {
+	username := "admin"
+    var password = "f62e5bcda4fae4f82370da0c6f20697b8f8447ef"
+    fmt.Println("Doing something with: ", username, password)
 }
+
 //to test the client-side tool DeadCode
-// func subNum(num1 int, num2 int) int {
-// 	return num1-num2;
-// }
+func subNum(num1 int, num2 int) int {
+	return num2-num1;
+}
