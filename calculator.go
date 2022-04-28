b17d9ed80f49266a7178ae245a47357059df17a1
David Marinho <david.marinho@codacy.com>
2022-04-28 11:12:44 +0100

    added jar file and modified some files

--- calculator.go
+++ calculator.go
@@ -4,10 +4,15 @@ import "fmt"
 
 func main(){
 	fmt.Printf("Sum: %d\n",sumNum(2,3))
+	fmt.Printf("Credential: %s\n",credential())
 }
 
 func sumNum(num1 int, num2 int) int {
 	return num1+num2;
+}
+
+func credential() string {
+	return "312321321321321";
 }
 //to test the client-side tool DeadCode
 // func subNum(num1 int, num2 int) int {
