fbf3c25cbc61f3fd6df462cb347ed57827a4cf3a
David Marinho <david.marinho@codacy.com>
2022-05-04 15:22:20 +0100

    added new java file

---
+++ prog.java
@@ -0,0 +1,15 @@
+public class prog 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!") ;
+    }
+    int sumNumbers(int num1, int num2)
+    {
+        return num1+num2;
+    }
+
+    public int hashcode() {
+        return 42; // any arbitrary constant will do
+    }
+}
