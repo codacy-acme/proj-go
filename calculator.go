34ef9e53a4f3f1aec3067bee30204143b96ea510
David Marinho <david.marinho@codacy.com>
2022-05-09 17:53:28 +0100

    calculator updated

--- calculator.go
+++ calculator.go
@@ -7,14 +7,19 @@ import (
 
 func main(){
 	fmt.Printf("Sum: %d\n",sumNum(2,3))
-	credential()
-	sleep()
+	//credential()
+	//sleep()
+	var guess *int
+	a := 12345
+	guess = &a
+	fn(guess)
 }
 
 func sumNum(num1 int, num2 int) int {
 	return num1+num2;
 }
 
+//to test an issue
 func credential() {
 	username := "admin"
     var password = "f62e5bcda4fae4f82370da0c6f20697b8f8447ef"
@@ -26,8 +31,17 @@ func subNum(num1 int, num2 int) int {
 	return num2-num1;
 }
 
-//to test the client-side tool StaticCheck
+//to test the client-side tool StaticCheck - SA1004
 func sleep() {
     time.Sleep(8)
     fmt.Println("Sleep Over.....")
+}
+
+func fn(x *int) {
+    fmt.Println(*x)
+
+    // This nil check is equally important for the previous dereference
+    if x != nil {
+        fmt.Println("ERROR")
+    }
 }
